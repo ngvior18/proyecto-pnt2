4ff672ba2c7034c06bc4b8cacd2af48b82fccc25
TGallo11 <tomasgallo724@gmail.com>
2024-06-24 00:47:00 -0300

    catalogo conectado, falta diseño

--- src/app/categorias/Categoria.jsx
+++ src/app/categorias/Categoria.jsx
@@ -1,14 +1,13 @@
-
 export default function Categoria(props) {
-	return (
-		<div>
-			<li>
-				<div>
-					<div>
-						<h2>{props.nombre}</h2>
-						</div>
-                    </div>
-			</li>
-		</div>
-	);
+  return (
+    <div>
+      <li>
+        <div>
+          <div>
+            <h2>{props.nombre}</h2>
+          </div>
+        </div>
+      </li>
+    </div>
+  );
 }

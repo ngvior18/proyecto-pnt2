0fa730b8b11458e91c6dcc6be88f50e87c4981d5
ngvior <141837800+ngvior18@users.noreply.github.com>
2024-06-23 23:50:23 -0300

    Revert "CRUD de Categorias y Usuarios"

--- src/app/ui/ButtonCRUD.jsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-export default function ButtonCRUD({ textButton, classNameExtra, onClick }) {
-
-    return (
-        <button className={`buttonCrud ${classNameExtra}`} onClick={onClick}>{textButton}</button>
-    )
-
-}
--- src/app/ui/InputCRUD.jsx
+++
@@ -1,6 +0,0 @@
-export default function InputCRUD({ classNameExtra, placeholder, type }) {
-
-    return (
-        <input type={type} className={`inputCRUD ${classNameExtra}`} placeholder={placeholder} />)
-
-}
